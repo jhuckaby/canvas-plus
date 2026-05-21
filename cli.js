367cc528765d37ffa06e81ad4aab9772c651ae5e
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-05-20 19:04:09 -0700

    Enhance CLI functionality with improved filter handling and composite support

--- cli.js
+++ cli.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 // Very simple CLI wrapper around canvas-plus
+//
 // Beware: This will REPLACE local images with the specified filters applied, unless "--output FILE" is specified.
+// Or if you change format with `--filter "write/format:webp"` this will adjust the output filenames as well.
+// Also, if you specify `--dir /path/to/dir` this will write the output files into the specified directory.
+// Add `--verbose` to include full debug ouptut from canvas-plus.
+//
 // Usage: ./cli.js FILE1 [FILE2 ...] --filter "resize/width:400/height:300" --filter "adjust/hue:-60" --filter "write/quality:90"
 
+var Path = require('path');
 var fs = require('fs');
 var cli = require('pixl-cli');
 var CanvasPlus = require('./main.js');
@@ -18,6 +24,7 @@ var usage = `Usage: ./cli.js FILE1 [FILE2 ...] --filter "resize/width:400/height
 
 if (!files || !files.length) die(usage);
 if (!args.filter) die(usage);
+if (args.output && (files.length > 1)) die("Error: Cannot use `--output` argument with multiple input files.  Consider `--dir` instead.\n");
 
 var filters = [];
 Tools.alwaysArray(args.filter).forEach( function(raw) {
@@ -35,6 +42,15 @@ Tools.alwaysArray(args.filter).forEach( function(raw) {
 		else if (value.match(/^\-?\d+$/)) value = parseInt(value);
 		else if (value.match(/^true$/)) value = true;
 		else if (value.match(/^false$/)) value = false;
+		
+		if (key.match(/^(clip|rect|line|rgb|red|green|blue|alpha|matrix)$/)) {
+			// parse CSV into arrays for specific param names
+			value = value.split(/\,/).map( function(num) { return parseFloat(num); } );
+		}
+		if ((key == 'clip') && Array.isArray(value)) {
+			// clip has a special syntax {x/y/width/height}
+			value = { x: value[0], y: value[1], width: value[2], height: value[3] };
+		}
 		
 		params[ key ] = value;
 	});
@@ -58,30 +74,51 @@ async.eachSeries( files,
 			if (err) return callback(err);
 			var write_opts = {};
 			
-			filters.forEach( function(filter) {
-				var name = filter._name;
-				var params = Tools.copyHashRemoveKeys(filter, { _name: 1 });
-				
-				if (name == 'write') write_opts = params;
-				else canvas[name](params);
-				
-				if (canvas.getLastError()) {
-					die("Failed to process image: " + file + ": " + name + ":" + err + "\n");
-				}
-			}); // forEach filter
-			
-			// write
-			canvas.write( write_opts, function(err, buf) {
-				if (err) return callback(err);
-				
-				fs.writeFile( args.output || file, buf, function(err) {
-					if (err) return callback(err);
+			async.eachSeries( filters,
+				function(filter, callback) {
+					var name = filter._name;
+					var params = Tools.copyHashRemoveKeys(filter, { _name: 1 });
 					
-					print("Wrote: " + (args.output || file) + "\n");
-					canvas.logDebug(3, "Performance Metrics: ", canvas.getMetrics() );
-					callback();
-				});
-			} );
+					if (name == 'write') write_opts = params;
+					else if (name == 'composite') return handleComposite(canvas, params, callback);
+					else if (name == 'mask') {
+						params.mode = 'destination-in';
+						return handleComposite(canvas, params, callback);
+					}
+					else canvas[name](params);
+					
+					if (canvas.getLastError()) {
+						die("Failed to process image: " + file + ": " + name + ":" + canvas.getLastError() + "\n");
+					}
+					
+					process.nextTick(callback);
+				},
+				function(err) {
+					if (err) die("Error: " + err + "\n");
+					
+					// write
+					canvas.write( write_opts, function(err, buf) {
+						if (err) return callback(err);
+						
+						// if changing format, also change file ext
+						if (write_opts.format) file = file.replace(/\.\w+$/, '.' + write_opts.format.replace(/jpeg/, 'jpg'));
+						
+						// if dir specified, change file path
+						if (args.dir) file = Path.join( args.dir, Path.basename(file) );
+						
+						// insert optional appendage before file ext
+						if (args.append) file = file.replace(/(\.\w+)$/, args.append + '$1');
+						
+						fs.writeFile( args.output || file, buf, function(err) {
+							if (err) return callback(err);
+							
+							print("Wrote: " + (args.output || file) + "\n");
+							canvas.logDebug(3, "Performance Metrics: ", canvas.getMetrics() );
+							callback();
+						});
+					} ); // write
+				}
+			); // eachSeries
 		}); // load
 	},
 	function(err) {
@@ -89,3 +126,14 @@ async.eachSeries( files,
 		print("Done!\n");
 	}
 ); // eachSeries
+
+function handleComposite(canvas, params, callback) {
+	// handle composite (must load image from disk)
+	var img = new CanvasPlus();
+	img.load( params.image, function(err) {
+		if (err) return callback(err);
+		params.image = img;
+		canvas.composite(params);
+		callback( canvas.getLastError() );
+	} );
+};
